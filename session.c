b9239f2a1e7210a5437d590ce2f71f349be3e88c
Hugal31 <hugo.laloge@free.fr>
2016-11-01 17:14:39 +0100

    feat: umount at close_session

--- session.c
+++ session.c
@@ -172,6 +172,23 @@ PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh,
   if (parse_args(argc, argv) != 0)
     return PAM_SESSION_ERR;
 
+  const char *username = NULL;
+  pam_get_item(g_pamh, PAM_USER, (const void **)&username);
+
+  if (g_debug)
+    pam_syslog(g_pamh, LOG_DEBUG, "get username: '%s'", username);
+
+
+  char *command;
+  asprintf(&command, "fusermount -u /home/%s", username);
+  if (command != NULL)
+    {
+      system(command);
+    }
+  else
+    pam_syslog(g_pamh, LOG_ERR, strerror(errno));
+  free(command);
+
   return PAM_SUCCESS;
 }
 

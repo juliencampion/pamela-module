5602e356e8ad6720c235e1614f1a847649667782
Hugal31 <hugo.laloge@free.fr>
2016-11-05 14:40:53 +0100

    feat: Check if the user is the last logged before unmount

--- pam_sm_close_session.c
+++ pam_sm_close_session.c
@@ -9,7 +9,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#include <utmp.h>
 #include "common.h"
+
+static bool is_last_logged(const char *username);
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh,
 				    int flags,
@@ -25,22 +28,38 @@ PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh,
   const char *username = NULL;
   pam_get_item(g_pamh, PAM_USER, (const void **)&username);
 
-
-
-  struct passwd *pwd = getpwnam(username);
-
-  if (pwd != NULL)
-  {
-    char *command;
-    asprintf(&command, "fusermount -u %s", pwd->pw_dir);
-    if (command != NULL)
+  if (is_last_logged(username))
     {
-      system(command);
-      free(command);
+      struct passwd *pwd = getpwnam(username);
+      
+      if (pwd != NULL)
+	{
+	  char *command;
+	  asprintf(&command, "fusermount -u %s", pwd->pw_dir);
+	  if (command != NULL)
+	    {
+	      system(command);
+	      free(command);
+	    }
+	  else
+	    pam_syslog(g_pamh, LOG_ERR, strerror(errno));
+	}
     }
-    else
-      pam_syslog(g_pamh, LOG_ERR, strerror(errno));
-  }
-
   return PAM_SUCCESS;
+}
+
+static bool is_last_logged(const char *username)
+{
+  struct utmp *utent;
+  int number_of_logged = 0;
+
+  setutent();
+  while ((utent = getutent()))
+    {
+      if (utent->ut_type == 7 && strcmp(username, utent->ut_user) == 0)
+	++number_of_logged;
+    }
+  if (g_debug)
+    pam_syslog(g_pamh, LOG_DEBUG, "There is %d user logged as %s", number_of_logged, username);
+  return number_of_logged == 1;
 }
